71a22f7650364a8f896ba364c026b2e9cec4e7b8
raresteak <d20936974@gmail.com>
2017-06-05 06:13:15 -0500

    Update guessTheNumber.py

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -3,7 +3,6 @@
 #
 #  guessTheNumber.py
 #  
-#  Copyright 2017 Unknown <jar@blue>
 #  
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
