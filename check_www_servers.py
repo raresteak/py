064fd7cdad013f49d394753a56cda92b0b5710d9
raresteak <raresteak@users.noreply.github.com>
2018-03-14 09:55:09 -0500

    Add files via upload

--- check_www_servers.py
+++ check_www_servers.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # Check webservers
-import urllib.request
+import urllib.request, os
 
 WWWPORT='9088'
-WWWLIST='servers.txt'  # server/IP one per line
+WWWLIST='servers.txt'
 TIMEOUT=2
 
 servers2Check = open(WWWLIST, 'r')
@@ -13,6 +13,14 @@ for server in servers2Check:
 	WWW = str('https://' + server.strip() + ':' + WWWPORT)
 	try:
 		CODE = urllib.request.urlopen(WWW,None,TIMEOUT).getcode()
-		print(str(CODE) + ':' + WWW )
+		print(str(CODE) + ':' + WWW + " UP!")
 	except urllib.error.URLError:
-		print("Timeout:" + WWW + " HOST DOWN!!!!!!!!!!!!!!!!")
+		print("Timeout connecting to: " + WWW + "   Port:" + WWWPORT + " down!")
+		print("Trying to ping host:...")
+		pingHost=str('ping -n 1 ' + server.strip())
+		if not os.system(pingHost):
+			# If return code 0, successful ping
+			print("Host up, reponds to ping")
+		else:
+			# return is 1, ping failed
+			print("Host down!!!")
