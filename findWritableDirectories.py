f85b83f343573b2972ae2e1c56cf582cc5aaa556
raresteak <raresteak@users.noreply.github.com>
2021-12-03 08:58:08 -0600

    Fixed PEP8 issues

---
+++ findWritableDirectories.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python3
+# Finds writable directories on Windows and nix systems
+# Author Raresteak
+import os
+import random
+import string
+if os.name == 'nt':
+    Sep = "\\"
+else:
+    Sep = "/"
+path = input("Enter full directory path to start from: ").rstrip(Sep)
+print("Writable directories found from " + path)
+tmpFile = "tmp." + str("".join(random.sample(
+    string.ascii_uppercase + string.ascii_lowercase + string.digits, 8)))
+try:
+    testFile = os.path.join(path, tmpFile)
+    fh = open(testFile, 'w')
+    fh.close()
+    print(path)
+    os.remove(testFile)
+except PermissionError:
+    pass
+
+for (root, dirs, files) in os.walk(path):
+    for dir in enumerate(dirs):
+        testFile = os.path.join(root, dir[1], tmpFile)
+        try:
+            fh = open(testFile, 'w')
+            testPath = os.path.join(root, dir[1])
+            fh.close()
+            print(testPath)
+            os.remove(testFile)
+        except PermissionError:
+            pass
+        except OSError:
+            pass
